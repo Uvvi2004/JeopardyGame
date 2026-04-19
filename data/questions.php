ce51042ad5723a0692fb9f07f7e938eb579d15d4
Uddhav <uddhav3114@gmail.com>
2026-04-19 00:48:09 -0400

    Update questions.php

--- data/questions.php
+++ data/questions.php
@@ -1,7 +1,22 @@
 <?php
+
 $questions = [
-	"Movies" => [
-		["q" => "Who directed Titanic?", "a" => "James Cameron", "points" => 100]
-	]
+    "Movies" => [
+        ["id" => 1, "q" => "Who directed Titanic?", "a" => "James Cameron", "points" => 100],
+        ["id" => 2, "q" => "Main character in Iron Man?", "a" => "Tony Stark", "points" => 200],
+        ["id" => 3, "q" => "Avatar world name?", "a" => "Pandora", "points" => 300],
+        ["id" => 4, "q" => "Who is Batman?", "a" => "Bruce Wayne", "points" => 400]
+    ],
+    "TV Shows" => [
+        ["id" => 5, "q" => "Show with dragons?", "a" => "Game of Thrones", "points" => 100],
+        ["id" => 6, "q" => "Breaking Bad main character?", "a" => "Walter White", "points" => 200],
+        ["id" => 7, "q" => "Friends cafe name?", "a" => "Central Perk", "points" => 300],
+        ["id" => 8, "q" => "Stranger Things girl?", "a" => "Eleven", "points" => 400]
+    ],
+    "Sci-Fi" => [
+        ["id" => 9, "q" => "Star Wars desert planet?", "a" => "Tatooine", "points" => 100],
+        ["id" => 10, "q" => "Terminator AI?", "a" => "Skynet", "points" => 200],
+        ["id" => 11, "q" => "Alien ship name?", "a" => "Nostromo", "points" => 300],
+        ["id" => 12, "q" => "Matrix main character?", "a" => "Neo", "points" => 400]
+    ]
 ];
-?>
