3c1d947bb7db293a85d70f6e1b5c4cab4050376f
Juan Cazarez <jm.cazarez.sepulveda@gmail.com>
2026-04-19 18:37:56 -0400

    im trying to get the css to work on this page

--- game.php
+++ game.php
@@ -26,57 +26,61 @@ foreach ($questions as $qs) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Jeopardy Game</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
 
-<h1>Jeopardy Game</h1>
+    <h1>Jeopardy Game</h1>
 
-<h3>Score: <?php echo $_SESSION['score'] ?? 0; ?></h3>
+    <h3>Score: <?php echo $_SESSION['score'] ?? 0; ?></h3>
 
-<form method="POST">
-    <button type="submit" name="reset">Reset Game</button>
-</form>
+    <form method="POST">
+        <button type="submit" name="reset">Reset Game</button>
+    </form>
 
-<?php if (count($_SESSION['used_questions']) === $totalQuestions): ?>
-    <h2>Game Over!</h2>
-    <p>Final Score: <?php echo $_SESSION['score'] ?? 0; ?></p>
-<?php endif; ?>
+    <?php if (count($_SESSION['used_questions']) === $totalQuestions): ?>
+        <h2>Game Over!</h2>
+        <p>Final Score: <?php echo $_SESSION['score'] ?? 0; ?></p>
+    <?php endif; ?>
 
-<table border="1" cellpadding="20">
+    <table border="1" cellpadding="20">
 
-<tr>
-    <?php foreach ($questions as $category => $qs): ?>
-        <th><?php echo htmlspecialchars($category); ?></th>
-    <?php endforeach; ?>
-</tr>
+        <tr>
+            <?php foreach ($questions as $category => $qs): ?>
+                <th><?php echo htmlspecialchars($category); ?></th>
+            <?php endforeach; ?>
+        </tr>
 
-<?php for ($i = 0; $i < 4; $i++): ?>
-<tr>
-    <?php foreach ($questions as $category => $qs): ?>
-        <td>
-            <?php if (isset($qs[$i])): ?>
-                <?php if (!in_array($qs[$i]['id'], $_SESSION['used_questions'])): ?>
-                    <form action="question.php" method="GET">
-                        <input type="hidden" name="id" value="<?php echo $qs[$i]['id']; ?>">
-                        <button type="submit">
-                            <?php echo $qs[$i]['points']; ?>
-                        </button>
-                    </form>
-                <?php else: ?>
-                    ✔
-                <?php endif; ?>
-            <?php endif; ?>
-        </td>
-    <?php endforeach; ?>
-</tr>
-<?php endfor; ?>
+        <?php for ($i = 0; $i < 4; $i++): ?>
+            <tr>
+                <?php foreach ($questions as $category => $qs): ?>
+                    <td>
+                        <?php if (isset($qs[$i])): ?>
+                            <?php if (!in_array($qs[$i]['id'], $_SESSION['used_questions'])): ?>
+                                <form action="question.php" method="GET">
+                                    <input type="hidden" name="id" value="<?php echo $qs[$i]['id']; ?>">
+                                    <button type="submit">
+                                        <?php echo $qs[$i]['points']; ?>
+                                    </button>
+                                </form>
+                            <?php else: ?>
+                                ✔
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    </td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endfor; ?>
 
-</table>
+    </table>
 
-<br>
-<a href="dashboard.php">Back</a>
+    <br>
+    <a href="dashboard.php">Back</a>
 
 </body>
+
 </html>
