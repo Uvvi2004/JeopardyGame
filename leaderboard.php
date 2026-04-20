5e65fac0d54a48b00fd5c095358c334ead9a8652
Juan Cazarez <jm.cazarez.sepulveda@gmail.com>
2026-04-19 20:10:55 -0400

    added css for the leaderboard

--- leaderboard.php
+++ leaderboard.php
@@ -14,7 +14,7 @@ if (file_exists($file)) {
 }
 
 // sort high → low
-usort($scores, function($a, $b) {
+usort($scores, function ($a, $b) {
     return $b['score'] <=> $a['score'];
 });
 
@@ -24,31 +24,42 @@ $scores = array_slice($scores, 0, 10);
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Leaderboard</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
-<body>
 
-<h1>Leaderboard</h1>
+<body class="leaderboard-body">
 
-<table border="1" cellpadding="10">
-    <tr>
-        <th>Rank</th>
-        <th>User</th>
-        <th>Score</th>
-    </tr>
+    <h1>Leaderboard</h1>
 
-    <?php foreach ($scores as $index => $entry): ?>
+    <table border="1" cellpadding="10" class="leaderboard-table">
         <tr>
-            <td><?php echo $index + 1; ?></td>
-            <td><?php echo htmlspecialchars($entry['user']); ?></td>
-            <td><?php echo $entry['score']; ?></td>
+            <th>Rank</th>
+            <th>User</th>
+            <th>Score</th>
         </tr>
-    <?php endforeach; ?>
-</table>
 
-<br>
-<a href="dashboard.php">Back</a>
+        <?php foreach ($scores as $index => $entry): ?>
+            <tr class="<?php echo $index < 3 ? 'top-row' : ''; ?>">
+                <td class="rank-cell rank-<?php echo $index + 1; ?>">
+                    <?php
+                    if ($index === 0) echo '🥇';
+                    elseif ($index === 1) echo '🥈';
+                    elseif ($index === 2) echo '🥉';
+                    else echo $index + 1;
+                    ?>
+                </td>
+                <td><?php echo htmlspecialchars($entry['user']); ?></td>
+                <td><?php echo $entry['score']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+    <br>
+    <a href="dashboard.php">Back</a>
 
 </body>
+
 </html>
